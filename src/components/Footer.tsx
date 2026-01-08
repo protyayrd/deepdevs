507063603a631b0255d03c7356690dd00a1a5f60
Protyay Roy <protyay@Protyays-Mac-mini.local>
2026-01-08 17:51:37 +0600

    chore: update footer copyright year to 2026 and remove icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -168,15 +168,8 @@ export default function Footer({ content }: { content?: IFooterContent }) {
             {/* Copyright */}
             <div className="flex items-center gap-1 text-[16px] leading-[28px] text-[#24222e] font-roboto">
               <span>Copyright</span>
-              <span className="flex items-center justify-center w-4 h-4">
-                {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img
-                  src="/copyright-icon.png"
-                  alt="Copyright"
-                  className="w-full h-full object-contain"
-                />
-              </span>
-              <span>2025</span>
+              
+              <span>2026</span>
               <Link href="#" className="text-[#3e66f3] hover:underline">
                 DeepDevs
               </Link>
